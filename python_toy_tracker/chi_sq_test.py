754b3585147ab7d207a4d360797d6b08bf6d0a01
JSmeats <john.smeaton.13@ucl.ac.uk>
2016-11-22 23:29:04 +0000

    Toy tracker now accounts for spacing between straws, such that tracks can pass through layer without striking straw. Previously, much of code required a single hit in each layer.

--- python_toy_tracker/chi_sq_test.py
+++ python_toy_tracker/chi_sq_test.py
@@ -1,5 +1,6 @@
 import toytraceback as ttb
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 import random
 import sys
@@ -7,19 +8,28 @@ import math
 import scipy.stats as stats
 from scipy.optimize import curve_fit 
 
-track_count = 2 # Number of tracks to fit
+track_count = 10 # Number of tracks to fit
 module_alignment = 1.5 # Set up detector, and change alignment of first module
 fit_count = 500 # Number of times to run fit
 
-deg_freedom = 0 # Number of degrees of freedom in fit
+# Whether to include smearing of residuals, and finite straw size
+detector_smear = True
+detector_finite_straws = True
 
-alignment_residuals = []
-chi_squared_vals = []
+# Contain calculated chi^2 values, and alignment residuals 
+alignment_errors = dict()
+chi_squared_vals = dict()
+
+
+reduced_chi_squared_cut = 2
 
 print "Carrying out", fit_count, "fits, with", track_count, "tracks:"  
 
 for k in xrange(fit_count):
-    true_detector = ttb.Detector(smearing=True)
+    
+    print k
+
+    true_detector = ttb.Detector(smearing=detector_smear, finite_straws=detector_finite_straws)
     true_detector.set_module_x_align(1, module_alignment)
 
     # Get wire coordinates
@@ -31,121 +41,139 @@ for k in xrange(fit_count):
     x_top = [random.uniform(ttb.track_boundary_x_lower, ttb.track_boundary_x_upper) for i in xrange(track_count)]
     y_bottom = [ttb.track_boundary_y_lower for i in xrange(track_count)]
     y_top = [ttb.track_boundary_y_upper for i in xrange(track_count)]
-
-    # # Get boundaries on intercepts and gradients for fit, with large boundary on module alignment distance
-    # lower_fit_bounds = [0]*((2 * track_count) + 1)
-    # upper_fit_bounds = [0]*((2 * track_count) + 1)
-    # for i in xrange(len(lower_fit_bounds)):
-    #     if (i % 2 == 0):
-    #         lower_fit_bounds[i] = (ttb.track_boundary_x_lower - ttb.track_boundary_x_upper) / (ttb.track_boundary_y_upper - ttb.track_boundary_y_lower)
-    #         upper_fit_bounds[i] = (ttb.track_boundary_x_upper - ttb.track_boundary_x_lower) / (ttb.track_boundary_y_upper - ttb.track_boundary_y_lower)
-    #     else:
-    #         lower_fit_bounds[i] = ttb.track_boundary_x_lower
-    #         upper_fit_bounds[i] = ttb.track_boundary_x_upper
-    
-    # lower_fit_bounds[-1] = -np.inf
-    # upper_fit_bounds[-1] = np.inf
-    
-    # fit_bounds = (lower_fit_bounds, upper_fit_bounds)
-    # print fit_bounds
-    
     
     
     # Calculate track gradient and x-intercept from track coordinates
     gradient = [((x_top[i] - x_bottom[i]) / (y_top[i] - y_bottom[i])) for i in xrange(track_count)]
     intercept = [x_bottom[i] - (gradient[i] * y_bottom[i]) for i in xrange(track_count)]
-
+    
     # Set up track, and get pos of beginning, end points
     true_track = [ttb.Track(gradient[i], intercept[i]) for i in xrange(track_count)]
     x_true_track = [true_track[i].get_x_points() for i in xrange(track_count)]
     y_true_track = [true_track[i].get_y_points() for i in xrange(track_count)]
 
+    
     # Get wire hits in each layer, then assign these to detector
-    wire_hits = []
-    for i in xrange(track_count): 
-        wire_hits = wire_hits + (ttb.closest_hit_wires(true_detector, true_track[i]))
-        
-    true_detector.set_wire_hits(wire_hits)
+    events = []
+    for j in xrange(track_count): 
+        wire_hits = ttb.closest_hit_wires(true_detector, true_track[j])
+        event = ttb.DetectorHitEvent(true_track[j], wire_hits, j)
+        events.append(event)
 
-    # New detector object for fitting
-    fitting_detector = ttb.Detector(smearing=True)
-    fitting_detector.set_wire_hits(wire_hits)
 
-    # Get x-displacement of all closest approached wires
-    hit_rads = [wire_hit.get_hit_dist() for wire_hit in wire_hits]
-        
-    wire_keys = [str((i - (i % 8)) / 8) + "-" + str(i % 8) for i in xrange(8 * track_count)] # Numbers to index all layers
+        # New detector object for fitting
+        fitting_detector = ttb.Detector(smearing=detector_smear, finite_straws=detector_finite_straws)
+        fitting_detector.set_events(events)
 
+        # Get x-displacement of all closest approached wires
+        hit_rads = []
+        for event in events:
+            for wire_hit in event.wire_hits:
+                hit_rads.append(wire_hit.get_hit_dist())
+                
+    wire_keys = fitting_detector.get_hit_keys()
 
     guess = [0.0 for i in xrange((2 * track_count) + 1)] # Initial guess for fitted track gradient and intercept, and module alignment (spurious convergence without this)
-
-    print k
-
-    fit_sigmas = [ttb.hit_resolution]*len(hit_rads)
+    
+    
+    fit_sigmas = [ttb.hit_resolution]*len(hit_rads) # Uncertainties on hit radii, from detector hit resolution
 
     # Finds values for track gradient and intercept, by fitting to wire hit x-displacements
     popt, pcov = curve_fit(fitting_detector.get_hit_radius, wire_keys, hit_rads, p0=guess, sigma=fit_sigmas)
-    
-    alignment_residuals.append(popt[-1] - module_alignment)
+ 
+    # Calculate residuals and chi-squared for residuals, to test goodness of fit.
+    residuals = np.array(hit_rads) - np.array(fitting_detector.get_hit_radius(wire_keys, popt)) / 1.0
+    deg_freedom = len(residuals) - len(popt)
+    chi_squared = np.sum((residuals / fit_sigmas)**2)
 
-    # Calculate residuals and chi-squared, to test goodness of fit.
-    residuals = (np.array(hit_rads) - np.array(fitting_detector.get_hit_radius(wire_keys, popt))) / np.array(fit_sigmas)
-    deg_freedom = len(wire_keys) - len(popt) # Get number of degrees of freedom for this fit (should be same for each fit)
-    chi_squared = np.sum(residuals**2) #/ deg_freedom
 
     # Get alignment residual, and chi-squared value if reduced chi-squared less than 10
     # (Should filter out spurious fits)
-    if (chi_squared < 10 * deg_freedom):
-        chi_squared_vals.append(chi_squared) 
-        alignment_residuals.append(popt[-1] - module_alignment)
+    if (chi_squared < reduced_chi_squared_cut * deg_freedom):
 
-
-
-# print ""
-# print "Mean Residual:", np.mean(np.array(alignment_residuals)), "mm"
-# print "Standard Deviation:", np.std(np.array(alignment_residuals)), "mm"
-
-
-
-# # Plot histogram of residuals
-# plt.hist(alignment_residuals, bins=20)
-# plt.xlabel("Residual Value / mm")
-# plt.ylabel("Frequency")
-# plt.show()
-
-# Plot histogram of chi-squared, then get bin contents, and edges
-x_chi_plot = np.linspace(stats.chi2.ppf(0.01, deg_freedom), stats.chi2.ppf(0.99, deg_freedom), 100)
-chi_bin_conts, chi_bin_edges, chi_patches = plt.hist(chi_squared_vals, bins=20, label='Calculated Chi-Squared')
-
-# Get width of each histogram bin
-chi_bin_width = (chi_bin_edges[-1] - chi_bin_edges[0]) / len(chi_bin_conts)
-
-# Plot chi-squared distribution for given number of degrees of freedom in fit.
-plt.plot(x_chi_plot, len(chi_squared_vals) * chi_bin_width * stats.chi2.pdf(x_chi_plot, deg_freedom), 'r-', label=("Chi-Squared Dist (" + str(deg_freedom) + " dof)"))
-plt.legend(loc='best', frameon=False)
-plt.xlabel("Chi-Squared")
-plt.ylabel("Frequency")
-
-# Create arrays of number of fits with chi-squared values within given bin, and number expected in given bin from chi-squared dist.
-chi_func_bins_unfiltered = np.array([len(chi_squared_vals) * (stats.chi2.cdf(chi_bin_edges[i+1], deg_freedom) - stats.chi2.cdf(chi_bin_edges[i], deg_freedom)) for i in xrange(len(chi_bin_conts))])
-chi_obs_bins_unfiltered = np.array(chi_bin_conts)
-
-
-# Filter out entries in arrays corresponding to bins with zero entries
-bin_count = len(chi_func_bins_unfiltered)
-chi_func_bins = np.array([chi_func_bins_unfiltered[x] for x in xrange(bin_count) if chi_obs_bins_unfiltered[x] > 0])
-chi_obs_bins = np.array([chi_obs_bins_unfiltered[x] for x in xrange(bin_count) if chi_obs_bins_unfiltered[x] > 0])
-
-chi_sigmas = np.sqrt(chi_obs_bins) # Calculate uncertainty of observed entries in histogram bins. (Assume Poisson stats for each bin.)
-
-# Calculate chi-squared between observed fit chi-squared values, and ideal distribution
-chi_squared_dist_chi_squared = np.sum(((chi_func_bins - chi_obs_bins) / chi_sigmas)**2)
+        if (deg_freedom in alignment_errors):
+            chi_squared_vals[deg_freedom].append(chi_squared) 
+            alignment_errors[deg_freedom].append(popt[-1] - module_alignment)
+        else:
+            chi_squared_vals[deg_freedom] = [chi_squared]
+            alignment_errors[deg_freedom] = [popt[-1] - module_alignment]
 
 print ""
-print "Number of bins used for test:", len(chi_obs_bins)
-print "Chi-squared of measured chi-squared values to chi-squared distribution:", chi_squared_dist_chi_squared
-print "P(chi^2):", 1 - stats.chi2.cdf(chi_squared_dist_chi_squared, len(chi_obs_bins))
 print ""
 
-plt.show()
+for deg_freedom in alignment_errors:
+
+    print "*******************************************"
+    print "Fits With", deg_freedom, "Degrees of Freedom"
+    print "*******************************************"
+    print ""
+    print "Mean Alignment Error:", np.mean(np.array(alignment_errors[deg_freedom])), "mm"
+    print "Standard Deviation:", np.std(np.array(alignment_errors[deg_freedom])), "mm"
+
+    # Plot histogram of residuals
+    plt.hist(alignment_errors[deg_freedom], bins=20)
+    plt.title("Plot of Alignment Fit Errors for Fits with $\chi^2_{red} < " + str(reduced_chi_squared_cut) + "$, with " + str(deg_freedom) + " d.o.f.")
+    plt.xlabel("Alignment Error Value / mm")
+    plt.ylabel("Frequency")
+
+    number_patch = mpatches.Patch(color='none', label=("Data Count: " + str(len(alignment_errors[deg_freedom]))))
+    mean_patch = mpatches.Patch(color='none', label=("$\mu$: " + str(np.mean(np.array(alignment_errors[deg_freedom])))))
+    std_patch = mpatches.Patch(color='none', label=("$\sigma$: " + str(np.std(np.array(alignment_errors[deg_freedom])))))
+
+
+    align_handles = [number_patch, mean_patch, std_patch]
+    align_labels = [number_patch.get_label(), mean_patch.get_label(), std_patch.get_label()]
+    plt.legend(align_handles, align_labels, loc='best', frameon=False)
+
+    plt.show()
+
+    # Plot histogram of chi-squared, then get bin contents, and edges
+    x_chi_plot = np.linspace(stats.chi2.ppf(0.01, deg_freedom), stats.chi2.ppf(0.99, deg_freedom), 100)
+    chi_bin_conts, chi_bin_edges, chi_patches = plt.hist(chi_squared_vals[deg_freedom], bins=20, label='Calculated Chi-Squared')
+
+    # Get width of each histogram bin
+    chi_bin_width = (chi_bin_edges[-1] - chi_bin_edges[0]) / len(chi_bin_conts)
+
+    # Plot chi-squared distribution for given number of degrees of freedom in fit.
+    chi_dist_plot = plt.plot(x_chi_plot, len(chi_squared_vals[deg_freedom]) * chi_bin_width * stats.chi2.pdf(x_chi_plot, deg_freedom), 'r-', label=("Chi-Squared Dist (" + str(deg_freedom) + " dof)"))
+    plt.xlabel("Chi-Squared")
+    plt.ylabel("Frequency")
+    plt.title("Plot of $\chi^2$ for Fits with $\chi^2_{red} < " + str(reduced_chi_squared_cut) + "$, with " + str(deg_freedom) + " d.o.f., Compared to Ideal Distribution")
+
+    
+    # Create arrays of number of fits with chi-squared values within given bin, and number expected in given bin from chi-squared dist.
+    chi_func_bins_unfiltered = np.array([len(chi_squared_vals[deg_freedom]) * (stats.chi2.cdf(chi_bin_edges[i+1], deg_freedom) - stats.chi2.cdf(chi_bin_edges[i], deg_freedom)) for i in xrange(len(chi_bin_conts))])
+    chi_obs_bins_unfiltered = np.array(chi_bin_conts)
+
+
+    # Filter out entries in arrays corresponding to bins with zero entries
+    bin_count = len(chi_func_bins_unfiltered)
+    chi_func_bins = np.array([chi_func_bins_unfiltered[x] for x in xrange(bin_count) if chi_obs_bins_unfiltered[x] > 0])
+    chi_obs_bins = np.array([chi_obs_bins_unfiltered[x] for x in xrange(bin_count) if chi_obs_bins_unfiltered[x] > 0])
+
+    chi_sigmas = np.sqrt(chi_obs_bins) # Calculate uncertainty of observed entries in histogram bins. (Assume Poisson stats for each bin.)
+
+    # Calculate chi-squared between observed fit chi-squared values, and ideal distribution
+    chi_squared_dist_chi_squared = np.sum(((chi_func_bins - chi_obs_bins) / chi_sigmas)**2)
+
+    print ""
+    print "Number of bins used for test:", len(chi_obs_bins)
+    print "Chi-squared of measured chi-squared values to chi-squared distribution:", chi_squared_dist_chi_squared
+    print "P(chi^2):", 1 - stats.chi2.cdf(chi_squared_dist_chi_squared, len(chi_obs_bins))
+    print ""
+
+    # Patches to show chi-squared, number of entries in legend
+    number_patch = mpatches.Patch(color='none', label=("Data Count: " + str(len(chi_squared_vals[deg_freedom]))))
+    chi_squared_patch = mpatches.Patch(color='none', label=("$\chi^2_{red}$: " + str(chi_squared_dist_chi_squared / len(chi_obs_bins))))
+
+    # Sort out handles and labels, then create legend, and show plot
+    chi_handles, chi_labels = plt.subplot(111).get_legend_handles_labels()
+    chi_handles.append(number_patch)
+    chi_handles.append(chi_squared_patch)
+    chi_labels.append(number_patch.get_label())
+    chi_labels.append(chi_squared_patch.get_label())
+
+    plt.legend(chi_handles, chi_labels, loc='best', frameon=False)
+
+    plt.show()
 
--- python_toy_tracker/tracker_test.py
+++ python_toy_tracker/tracker_test.py
@@ -1,6 +1,7 @@
 import toytraceback as ttb
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+import matplotlib.patches as mpatches
 import numpy as np
 import random
 import sys
@@ -11,9 +12,13 @@ from scipy.optimize import curve_fit
 # Number of tracks to fit
 track_count = 50
 
+# Whether to include smearing of residuals, and finite straw size
+detector_smear = True
+detector_finite_straws = False
+
 # Set up detector, and change alignment of first module
 module_alignment = 1.5
-true_detector = ttb.Detector(smearing=True)
+true_detector = ttb.Detector(smearing=detector_smear, finite_straws=detector_finite_straws)
 true_detector.set_module_x_align(1, module_alignment)
 
 # Get wire coordinates
@@ -38,26 +43,29 @@ y_true_track = [true_track[i].get_y_points() for i in xrange(track_count)]
 
 
 # Get wire hits in each layer, then assign these to detector
-wire_hits = []
+events = []
 for j in xrange(track_count): 
-    wire_hits = wire_hits + (ttb.closest_hit_wires(true_detector, true_track[j]))
+    wire_hits = ttb.closest_hit_wires(true_detector, true_track[j])
+    event = ttb.DetectorHitEvent(true_track[j], wire_hits, j)
+    events.append(event)
 
-true_detector.set_wire_hits(wire_hits)
 
 # New detector object for fitting
-fitting_detector = ttb.Detector(smearing=True)
-fitting_detector.set_wire_hits(wire_hits)
+fitting_detector = ttb.Detector(smearing=detector_smear, finite_straws=detector_finite_straws)
+fitting_detector.set_events(events)
 
 # Get x-displacement of all closest approached wires
-hit_rads = [wire_hit.get_hit_dist() for wire_hit in wire_hits]
+hit_rads = []
+for event in events:
+    for wire_hit in event.wire_hits:
+        hit_rads.append(wire_hit.get_hit_dist())
 
-wire_keys = [str((i - (i % 8)) / 8) + "-" + str(i % 8) for i in xrange(8 * track_count)] # Numbers to index all layers
-
+wire_keys = fitting_detector.get_hit_keys()
 
 guess = [0.0 for i in xrange((2 * track_count) + 1)] # Initial guess for fitted track gradient and intercept, and module alignment (spurious convergence without this)
 
 print ""
-print "True Alignment:", module_alignment
+print "True Alignment:", module_alignment, "mm"
 print ""
 print "Fitting:"
 
@@ -68,7 +76,7 @@ popt, pcov = curve_fit(fitting_detector.get_hit_radius, wire_keys, hit_rads, p0=
 print "Complete"
 print ""
 
-print "Fitted Alignment:", popt[-1]
+print "Fitted Alignment:", popt[-1], "mm"
 
 # Calculate residuals and chi-squared for residuals, to test goodness of fit.
 residuals = np.array(hit_rads) - np.array(fitting_detector.get_hit_radius(wire_keys, popt)) / 1.0
@@ -81,8 +89,8 @@ print ""
 print ""
 print "Hit Residual Goodness of Fit Testing:"
 print ""
-print "Hit Residual Mean:", np.mean(residuals)
-print "Hit Residual Std-Dev:", np.std(residuals)
+print "Hit Residual Mean:", np.mean(residuals), "mm"
+print "Hit Residual Std-Dev:", np.std(residuals), "mm"
 print ""
 print "Number of Hit Residuals:", len(residuals)
 print "Number of Constraining Parameters", len(popt) 
@@ -95,8 +103,15 @@ x_fitted_track = [fitted_track[i].get_x_points() for i in xrange(track_count)]
 y_fitted_track = [fitted_track[i].get_y_points() for i in xrange(track_count)]
 
 # Get coordinates of wires hit in fitted detector
-x_hit_wires = [wire_hit.get_wire().get_absolute_x() for wire_hit in fitting_detector.get_wire_hits()]
-y_hit_wires = [wire_hit.get_wire().get_absolute_y() for wire_hit in fitting_detector.get_wire_hits()]
+x_hit_wires = []
+y_hit_wires = []
+
+
+for event in fitting_detector.get_events():
+    for wire_hit in event.wire_hits:
+        x_hit_wires.append(wire_hit.get_wire().get_absolute_x())
+        y_hit_wires.append(wire_hit.get_wire().get_absolute_y())
+
 
 # Get coordinates of all wires in fitted detector
 x_fitted_wires = fitting_detector.get_wires_x()
@@ -104,17 +119,36 @@ y_fitted_wires = fitting_detector.get_wires_y()
 
 
 # Plot points where wires located in true and fitted detector, where wires hit in fitted detector, lines showing true track and fitted track
-plt.subplot(1, 2, 1)
+geom_ax = plt.subplot(1, 2, 1)
 plt.scatter(x_true_wires, y_true_wires, color='red')
 plt.scatter(x_fitted_wires, y_fitted_wires, color='blue')
-plt.scatter(x_hit_wires, y_hit_wires, color='green')
+plt.scatter(x_hit_wires, y_hit_wires, color='green', marker='*', s=100)
 [plt.plot(x_true_track[i], y_true_track[i], 'r') for i in xrange(track_count)]
 [plt.plot(x_fitted_track[i], y_fitted_track[i], 'g') for i in xrange(track_count)]
+
+# Plot circles representing outside of straws, if using finite straw widths.
+if (detector_finite_straws):
+    for i in xrange(len(x_true_wires)):
+        circle = mpatches.Circle((x_true_wires[i], y_true_wires[i]), ttb.straw_rad, facecolor='none', edgecolor='r', alpha=0.5)
+        geom_ax.add_patch(circle)
+
+# Add title
+plt.title(str(track_count) + " Track Fit - Smearing: " + str(detector_smear) + " - Finite Straws: " + str(detector_finite_straws))
 
 # Label axes, and show plot
 plt.axis('equal')
 plt.xlabel("x-position / mm")
 plt.ylabel("y-position / mm")
+
+hits_patch = mpatches.Patch(color='none', label=("Hits: " + str(len(residuals))))
+chi_squared_patch = mpatches.Patch(color='none', label=("$\chi^2_{red}$: " + str(chi_squared / deg_freedom)))
+true_align_patch = mpatches.Patch(color='none', label=("True al.: " + str(module_alignment)))
+fit_align_patch = mpatches.Patch(color='none', label=("Fit al.: " + str(popt[-1])))
+
+diag_handles = [hits_patch, chi_squared_patch, true_align_patch, fit_align_patch]
+diag_labels = [hits_patch.get_label(), chi_squared_patch.get_label(), true_align_patch.get_label(), fit_align_patch.get_label()]
+
+plt.legend(diag_handles, diag_labels, loc='best', frameon=False)
 
 # Filter large residuals for fitting of gaussian function
 filtered_residuals = [x for x in residuals if abs(x) < (10 * ttb.hit_resolution)]
@@ -122,7 +156,6 @@ filtered_residuals = [x for x in residuals if abs(x) < (10 * ttb.hit_resolution)
 # Get mean and std-dev of fitted gaussian, then array of x-values for plotting
 (mu_res, sigma_res) = stats.norm.fit(filtered_residuals)
 gaus_x_res = np.linspace(mu_res - 4 * sigma_res, mu_res + 4 * sigma_res, 500)
-
 
 # Plot histogram of residuals
 plt.subplot(1, 2, 2)
@@ -138,8 +171,8 @@ plt.ylabel("Frequency")
 print ""
 print "Gaussian Goodness-of-Fit Testing:"
 print ""
-print "Fitted Gaussian Mean:", mu_res
-print "Fitted Gaussian Std-Dev:", sigma_res
+print "Fitted Gaussian Mean:", mu_res, "mm"
+print "Fitted Gaussian Std-Dev:", sigma_res, "mm"
 print ""
 
 # Get expected number of residuals in each bin, from fitted gaussian
@@ -159,5 +192,16 @@ print "Number of bins used for test:", len(res_obs_bin_contents)
 print "Reduced Chi-squared of measured residuals to fitted gaussian:", chi_squared_fitted_gauss / len(res_obs_bin_contents)
 print "P(chi^2):", 1 - stats.chi2.cdf(chi_squared_fitted_gauss, len(res_obs_bin_contents))
 print ""
+
+# Patches to show chi-squared, number of entries in legend
+number_patch = mpatches.Patch(color='none', label=("N: " + str(sum(res_obs_bin_contents))))
+chi_squared_patch = mpatches.Patch(color='none', label=("$\chi^2_{red}$: " + str(chi_squared_fitted_gauss / len(res_obs_bin_contents))))
+mean_patch = mpatches.Patch(color='none', label=("$\mu$: " + str(mu_res)))
+std_patch = mpatches.Patch(color='none', label=("$\sigma$: " + str(sigma_res)))
+
+# Sort out handles and labels, then create legend, and show plot
+hist_handles = [number_patch, chi_squared_patch, mean_patch, std_patch]
+hist_labels = [number_patch.get_label(), chi_squared_patch.get_label(), mean_patch.get_label(), std_patch.get_label()]
+plt.legend(hist_handles, hist_labels, loc='best', frameon=False)
 
 plt.show()
