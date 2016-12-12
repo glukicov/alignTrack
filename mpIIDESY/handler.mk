ce0d4becb33636fce145021d94d3b0e74451b889
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2016-12-12 12:26:30 +0000

    Add files via upload

---
+++ mpIIDESY/handler.mk
@@ -0,0 +1,25 @@
+PROGNAME      = MilleHandler
+SOURCES       = MilleHandler.cpp
+INCLUDES      = 
+OBJECTS       = $(patsubst %.cpp, %.o, $(SOURCES))
+ROOTCFLAGS   := $(shell root-config --cflags)
+ROOTLIBS     := $(shell root-config --libs)
+ROOTGLIBS    := $(shell root-config --glibs)
+ROOTLIBS     := $(shell root-config --nonew --libs)
+CFLAGS       += $(ROOTCFLAGS)
+LIBS         += $(ROOTLIBS)
+#
+LIBS         += -lMinuit
+LDFLAGS       = -O
+
+$(PROGNAME):    $(OBJECTS)
+		g++ -o $@ $(OBJECTS) $(LDFLAGS) $(LIBS) -std=c++11
+
+%.o : %.cpp $(INCLUDES)
+	g++ ${CFLAGS} -c  -g -o $@ $<
+
+test:
+	@echo $(ROOTCFLAGS)
+
+clean:	
+	-rm -f ${PROGNAME} ${OBJECTS}
