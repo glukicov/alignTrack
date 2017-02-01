9d6830ebdab4cb92854e5dd2d6c02d7b7d70ed40
JSmeats <john.smeaton.13@ucl.ac.uk>
2017-02-01 16:17:04 +0000

    Altered random number generation script to allow it to generate normally distributed random numbers.

--- mpIIDESY/randomGenerator.py
+++ mpIIDESY/randomGenerator.py
@@ -1,9 +1,11 @@
 #
-# Script to generate a plaintext file of random floating-point numbers between 0
-# and 1. Uses Marsenne Twister algorithm included in Python's random package. 
-# User can specify seed, number of random numbers to generate, and output file.
+# Script to generate a plaintext file of random floating-point numbers, either 
+# uniformly distributed between 0 and 1, or normally distributed with a mean 
+# of 0, and a standard deviation of 1. Uses Marsenne Twister algorithm included 
+# in Python's random package. User can specify seed, number of random numbers 
+# to generate, and output file.
 #
-# John Smeaton 27/01/2017
+# John Smeaton 01/02/2017
 #
 
 import sys
@@ -14,21 +16,24 @@ import random
 
 # Get system arguments, define string showing help message
 argv = sys.argv[1:]
-helpstring = "randomGenerator.py -s <seed> -o <output_file> -n <count>"
+helpstring = "randomGenerator.py -s <seed> -o <output_file> -n <count> -u <uniform> -g <gaussian>"
 
 # Initial values for output filename, random number seed, and count of random numbers to generate
 output_filename = os.path.abspath("./randoms_pre_gen.txt")
 seednum = 12345678
 count = 2020200
 
+uniform = False
+gaussian = False
+
 # Get options, arguments
 try:
-    opts, args = getopt.getopt(argv, "h:s:o:n:", ["help", "seed", "output_file", "count"])
+    opts, args = getopt.getopt(argv, "h:s:o:n:u:g:", ["help", "seed", "output_file", "count", "uniform", "gaussian"])
 except getopt.GetoptError:
     print helpstring
     sys.exit(2)
 
-# Set appropriate values for seed, filename, and count from user arguments.
+# Set appropriate values for seed, filename, count, and whether to generate uniform or gaussian distribution, from user arguments.
 for opt, arg in opts:
     if opt in ("-h", "--help"):
         print helpstring
@@ -41,6 +46,24 @@ for opt, arg in opts:
             sys.exit(2)
     elif opt in ("-o", "--output_file"):
         output_filename = os.path.abspath(arg)
+    elif opt in ("-u", "--uniform"):
+        if arg == "True":
+            uniform = True
+        elif arg == "False":
+            uniform = False
+    elif opt in ("-g", "--gaussian"):
+        if arg == "True":
+            gaussian = True
+        elif arg == "False":
+            gaussian = False
+
+
+if (uniform and gaussian):
+    print "Please flag either uniform or gaussian, not both"
+    sys.exit(2)
+
+if ((not uniform) and (not gaussian)):
+    uniform = True
 
 # Create directory for output file, if it doesn't already exist
 if not os.path.exists(os.path.dirname(output_filename)):
@@ -59,7 +82,12 @@ output_file = open(output_filename, "w")
 
 # Generate specified count of random numbers, writing to output file
 for i in xrange(count):
-    output_file.write(str(random.random()) + "\n")
+    
+    # Check if using uniform of gaussian distribution, writing appropriate random number to file
+    if (uniform):
+        output_file.write(str(random.random()) + "\n")
+    elif (gaussian):
+        output_file.write(str(random.normalvariate(0, 1)) + "\n")
 
 # Close output file
 output_file.close()
