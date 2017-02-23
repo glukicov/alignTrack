2f22bb69de8b15e45a4009a94dff255befc5ebc8
JSmeats <john.smeaton.13@ucl.ac.uk>
2017-02-23 10:36:39 +0000

    Implemented logger test

--- mpIIDESY/mptest1_port.cpp
+++ mpIIDESY/mptest1_port.cpp
@@ -28,6 +28,37 @@ struct Parameter_data {
  */
 int main(int argc, char* argv[]) {
 
+	try {
+		//Tell the logger to only show message at INFO level or above
+		Logger::Instance()->setLogLevel(Logger::INFO); 
+	
+		//Tell the logger to throw exceptions when ERROR messages are received
+		Logger::Instance()->enableCriticalErrorThrow();
+
+		//Send an INFO message (messages are just strings)
+		Logger::Instance()->write(Logger::INFO,"Hello from Logger");
+
+		//Send an INFO message with a number in it (make the string using a stringstream)
+		std::stringstream msg;
+		msg << "5.0 + 5.0 = " << (5.0+5.0);
+		Logger::Instance()->write(Logger::INFO,msg.str());
+
+		//Another way to send an INFO message with a number, using std::to_string to turn a number into a string
+		long double a = 1.0;
+		long double b = 6.0;
+		Logger::Instance()->write(Logger::INFO,"1.0 + 6.0 = " + std::to_string(a+b));
+
+		//Send an ERROR message, this will terminate the program
+		Logger::Instance()->write(Logger::ERROR,"Something terrible happened");
+
+	}
+
+	//Catch Logger exceptions
+	catch (CriticalError& e) {
+		std::cerr << "A critical error occurred, exiting" << std::endl;
+	}
+
+
 	cout << endl;
 	cout << "********************************************" << endl;
 	cout << "*                 MPTEST 1                 *" << endl;
