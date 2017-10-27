780b226637c1ca6e0b9ca801d1d5239a4e6c0e29
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2017-10-27 12:20:29 -0500

    Randoms

--- mpIIDESY/getRandoms.sh
+++ mpIIDESY/getRandoms.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 
+rand=$(expr $1 \* 16)
+norm=$(expr $1 \* 4)
 
-python randomIntGenerator.py -g True -o gaussian_ran.txt -s 987654321 -n $1
+python randomIntGenerator.py -g True -o gaussian_ran.txt -s 987654321 -n $rand
 
-python randomIntGenerator.py -u True -o uniform_ran.txt -s 123456789 -n $1
+python randomIntGenerator.py -u True -o uniform_ran.txt -s 123456789 -n $norm
