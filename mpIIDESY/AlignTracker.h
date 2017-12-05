f011635954161cba4da676d1bfa3500078444623
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2017-12-05 11:02:55 -0600

    Resolving conflicts with Circle_Fit branch

--- mpIIDESY/AlignTracker.h
+++ mpIIDESY/AlignTracker.h
@@ -2,27 +2,29 @@
 #define ALIGNTRACKER
 
 //This header file declares main function, and imports necessary dependencies for AlignTracker.cpp [description of purpose is there]
+#include "AlignTracker_helper.h" // helper functions
 #include "Mille.h"  // courtesy of Gero Flucke (DESY) 
 #include "Mille.cc" // courtesy of Gero Flucke (DESY) 
 #include "AlignTracker_methods.h" // Methods and functions for the main programme (AlignTracker.cpp)
 #include "Logger.hh"  // Logger courtesy of Tom Stuttard (UCL) - from gm2trackdaq repository
 
-//XXX Some includes maybe redundant 
+//XXX Some includes maybe redundant
 #include <iostream>
-#include <fstream> 
+#include <fstream>
 #include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
 #include <chrono>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <random>
 #include <iomanip>
 #include <chrono>
 #include <typeinfo>
 #include <stdlib.h>
 #include <stdio.h>
-#include <cmath> 
+#include <cmath>
 #include <TH1D.h>
-#include <TF1.h> 
+#include <TF1.h>
 #include <TH2D.h> //2D Histo Root class
 #include <TH3D.h> //3D Histo Root class
 #include <TFile.h> // data records for ROOT 
@@ -42,13 +44,14 @@
 #include <TText.h>
 #include <TLegend.h>
 #include <TGaxis.h>
-#include <TRatioPlot.h>
+//#include <TRatioPlot.h>
 #include <TGraph.h>
 #include <TGraphErrors.h>
 #include <TMarker.h>
 #include <TPaveStats.h>
+#include <TLatex.h>
+#include <TError.h>
 
-
-int main(int argc, char* argv[]);  
+int main(int argc, char* argv[]);
 
 #endif
