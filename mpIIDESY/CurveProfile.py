6a4b13b3383090fce3cd58eb993b7dbea7308039
glukicov <g.lukicov@ucl.ac.uk>
2019-06-21 16:43:13 -0500

    FoM Plots

--- mpIIDESY/CurveProfile.py
+++ mpIIDESY/CurveProfile.py
@@ -25,7 +25,7 @@ parser.add_argument('-c', '--coord', type=str, default="radial")
 parser.add_argument('-xmin', '--x_min', type=float, default=300)
 parser.add_argument('-xmax', '--x_max', type=float, default=3000)
 parser.add_argument('-ymin', '--y_min', type=float, default=-25)
-parser.add_argument('-ymax', '--y_max', type=float, default=11)
+parser.add_argument('-ymax', '--y_max', type=float, default=25)
 args = parser.parse_args()
 
 gROOT.SetBatch(1)
@@ -36,7 +36,7 @@ if not (mode == "profile" or mode== "graph"):
     sys.exit()
 
 coord = args.coord
-if not (coord == "radial" or coord== "vertical"):
+if not (coord == "radial" or coord== "vertical" or coord== "vertex"):
     print("Specify 'radial' or 'vertical' as --coord=")
     sys.exit()
 
@@ -56,28 +56,33 @@ gStyle.SetLegendTextSize(0.033)
 path =  "MomentumSlices/vertices/station"
 TfileName = "gm2tracker_MomSlices_ana.root"
 stationName = ["12", "18"]
-# topDir= "/Users/gleb/software/alignTrack/mpIIDESY/AlignCurvature_2D/" 
-states = ["run15922-15923_align", "sim_a=-0.5e-6", "sim_truth", "sim_a=1e-7", "sim_a=0.2e-6", "sim_a=0.3e-6", "sim_a=0.4e-6", "sim_a=0.5e-6", "sim_a=1e-6"]
+# states = ["run15922-15924_align", "sim_truth_default", "sim_truth_cbo"]
+states = ["run15922-15924_align", "sim_a=-0.5e-6", "sim_truth", "sim_a=1e-7", "sim_a=0.2e-6", "sim_a=0.3e-6", "sim_a=0.4e-6", "sim_a=0.5e-6", "sim_a=1e-6"]
 stateN=len(states)
-names = ["runs15922-15923 aligned", "sim_a=-0.5e-6", "sim_a=truth", "sim_a=0.1e-6", "sim_a=0.2e-6", "sim_a=0.3e-6", "sim_a=0.4e-6", "sim_a=0.5e-6", "sim_a=1.0e-6"]
+# names = ["data (aligned)", "sim default", "sim cbo"]
+names = ["data (aligned)", "sim a=-0.5e-6", "sim a=truth  ", "sim a=0.1e-6 ", "sim a=0.2e-6 ", "sim a=0.3e-6 ", "sim a=0.4e-6 ", "sim a=0.5e-6 ", "sim a=1.0e-6 "]
 
 #Containers to store histograms in orders as the names 
 
 colors = [1, 2, 3 ,4 ,5, 6 ,7 ,41 ,9] #purple, green 
 styles = [3001, 3002]
 plotName = ["h_"+coord+"Pos_vs_mom_timeCut", "h_"+coord+"Pos_vs_mom"]
+if (coord == "vertex"):
+    plotName = ["h_radialPos_vs_vertexExtrapDist_timeCut", "h_radialPos_vs_vertexExtrapDist"]
 plotYtitle = [coord+" beam position"]
 plotXtitle = ["P"]
+unitsX = ["MeV"]
 plotTitle = [" "]
 plotMean = ["<"+coord+">"]
 unitsY = ["mm"]
-unitsX = ["MeV"]
-
+if (coord == "vertex"):
+    plotXtitle = ["vertex extrapolation distance"]
+    unitsX = ["m"]
 
 #Make new canvas for plots 
-w = 1400
-h = 600
-c = TCanvas("h_radialPos_vs_mom_timeCut", "h_radialPos_vs_mom_timeCut", w, h)
+w = 1800
+h = 800
+c = TCanvas("c", "c", w, h)
 c.SetWindowSize(w + (w - c.GetWw()), h + (h - c.GetWh()))
 c.Divide(2,1)
 #Keep legend, histots and TFiles in scope 
@@ -88,11 +93,17 @@ profileArray=[]
 graphArray=[]
 meanArray=[] # for the final FoM shift-nominal 
 
+rows, cols = (len(stationName), stateN) 
+profile2DArray = [[0]*cols]*rows 
+
 i_total=0 # canvas id counter 
 for i_station in range(0, len(stationName)):
     #canvas pad and legend per station 
     c.cd(i_total+1)
+    #if(coord == "radial" or coord == "vertical"):
     legend =  TLegend(0.30,0.12,0.45,0.45)
+    # if(coord == "vertex"):
+    #     legend =  TLegend(0.10,0.45,0.45,0.9)
     legendArray.append(legend) # stroe all to keep in scope 
     for i_plot in range(0, stateN):
         
@@ -115,7 +126,7 @@ for i_station in range(0, len(stationName)):
 
        # print("before: ", plot.GetNbinsX())
         # Rebin2D(X,Y)
-        # plot.Rebin2D(4,1)
+        plot.Rebin2D(2,1)
         #print("after: ", plot.GetNbinsX())
 
         #Get the normalisation scale
@@ -134,7 +145,7 @@ for i_station in range(0, len(stationName)):
         binN_X=plot.GetXaxis().GetBinWidth(1)
         
         #make a profileX  
-        profile = plot.ProfileX("profile_"+str(i_plot)+str(i_station), 1, -1, "")
+        profile = plot.ProfileX("profile_S"+str(i_station)+"_"+str(i_plot), 1, -1, "")
         profileArray.append(profile)
         
         # create a new TGrpah for data with the same number of bins as the profile
@@ -178,15 +189,17 @@ for i_station in range(0, len(stationName)):
         tmp_holder[holder_i].GetXaxis().SetTitleSize(0.045)            
         tmp_holder[holder_i].GetXaxis().SetLabelSize(0.040)            
         tmp_holder[holder_i].GetYaxis().CenterTitle()
-        tmp_holder[holder_i].GetYaxis().SetTitleOffset(0.8)
+        tmp_holder[holder_i].GetYaxis().SetTitleOffset(1.15)
         tmp_holder[holder_i].GetYaxis().SetRangeUser(y_min, y_max) 
         tmp_holder[holder_i].GetXaxis().SetRangeUser(x_min, x_max) 
         tmp_holder[holder_i].SetTitle("S"+stationName[i_station])
+        gStyle.SetTitleY(0.95)
         tmp_holder[holder_i].GetXaxis().SetTitle(plotXtitle[0]+"/ "+str(round(binN_X,3))+" "+ unitsX[0])
         tmp_holder[holder_i].GetXaxis().CenterTitle()
         tmp_holder[holder_i].SetMarkerColor( colors[i_plot] )
         tmp_holder[holder_i].SetLineColor( colors[i_plot] )
         tmp_holder[holder_i].SetMarkerStyle(20)
+        tmp_holder[holder_i].SetMarkerSize(0.5)
 
         if (i_plot == 0):  
             if (mode == "profile"):
@@ -194,7 +207,6 @@ for i_station in range(0, len(stationName)):
             if (mode == "graph"):
                 gr.Draw("AP")
                 #gr.SetMarkerSize(2)
-                gr.SetMarkerStyle(20)
         else:
             if (mode == "profile"):
                 profile.Draw("E same")  # profile  
@@ -218,4 +230,66 @@ for i_station in range(0, len(stationName)):
 
 c.Draw()
 c.Print(coord+"_"+mode+"_"+"Pos_vs_mom_timeCut.png")
-c.SaveAs(coord+"_"+mode+"_"+"Pos_vs_mom_timeCut.C")
+# c.SaveAs(coord+"_"+mode+"_"+"Pos_vs_mom_timeCut.C")
+
+profile2DArray[0]=profileArray[0:9]
+profile2DArray[1]=profileArray[9:]
+#Calculate the sigma bands
+
+S_array=[]
+
+for i_station in range(0, len(stationName)):
+    print(stationName[i_station])
+    #print(profile2DArray[i_station])
+    for i_state in range(1, stateN):
+        data=profile2DArray[i_station][0]
+        sim=profile2DArray[i_station][i_state]
+        #loop over bins 
+        bin_number_X = data.GetNbinsX()
+        #print(bin_number_X)
+        i_total_bins = 0
+        S_mean=0
+        for i_bin_x in range(0, bin_number_X):
+            bin_content = data.GetBinContent(i_bin_x)
+            #skip empty bins 
+            if (bin_content == 0):
+                continue
+            #skip bins at the start (below the cut)":
+            bin_center=data.GetBinCenter(i_bin_x)
+            #print(bin_center)
+            if (bin_center < x_min):
+                continue
+            #once we hit the final bin, stop:
+            if (bin_center >= x_max):
+                break
+
+            #get the bin data 
+            data_r = data.GetBinContent(i_bin_x)
+            sim_r = sim.GetBinContent(i_bin_x)
+            data_er=data.GetBinError(i_bin_x)
+            sim_er=sim.GetBinError(i_bin_x)
+            # print(data_r, sim_r, data_er, sim_er)
+            # calculate the sigma: S = (dR)/E2, where quadrature sum of errors 
+            dR = sim_r - data_r
+            E2 = np.sqrt(data_er**2 + sim_er**2)
+            S = dR/E2
+            S_mean+=S
+            i_total_bins+=1
+        
+        #done with a state 
+        S_mean=S_mean/i_total_bins
+        S_array.append(S_mean)
+        #print(i_total_bins)
+
+
+rows, cols = (len(stationName), stateN-1) 
+S_array_2D = [[0]*cols]*rows 
+S_array_2D[0]=S_array[0:8]
+S_array_2D[1]=S_array[8:]    
+
+
+print("For range:", x_min, "to", x_max, "MeV:")
+for i_station in range(0, len(stationName)):
+    print(stationName[i_station])
+    for i_state in range(0, stateN-1):
+        print(names[i_state+1],": ", round(S_array_2D[i_station][i_state],3))
--- mpIIDESY/ToyGun.py
+++ mpIIDESY/ToyGun.py
@@ -5,7 +5,117 @@
 import argparse, sys
 import numpy as np 
 import matplotlib.pyplot as plt
+import subprocess, glob
 
-#define constants
-moduleN=8
-strawN=8 # 32 
+#### Define constants ###
+#Tracker geometry 
+ModuleN=4 # TODO 8 
+StrawN=8 # TODO 32
+Z_seperation =  0.1 # m  / 1000 mm between gun plane and the first plane of the tracker 
+X_spread = 0.015 # m / 150 mm +/- spread on the gun plane 
+
+strawSpacing = 0.606;  # x distance between straws in a layer
+layerSpacing = 0.515; # z distance between layers in a view
+viewSpacing = 2.020; # z distance between views in a modules
+moduleSpacing = 13.735; # z distance between modules' first layers [first layer of module 1 and first layer of module 2]
+layerDisplacement = 0.303; # relative x distance between first straws in adjacent layers in a view [upstream layer is +x shifted]
+
+hitSmearing = 0.00015 # m 150 um 
+
+#Physics 
+C = 2.99792458 * 1E8  # mm/s 
+q = 1.60217 * 1E-19  # C 
+B = 1.45147 # T //  assuming constant field  
+
+P_max = 3100 # MeV
+
+# create a defined seed RNG
+np.random.seed(151214)
+
+#Define plotting constants
+COLOR="green"
+
+### Command line input 
+parser = argparse.ArgumentParser()
+parser.add_argument("--nTracks", type=int, help="Number of tracks to generate")
+args = parser.parse_args()
+
+nTracks=args.nTracks
+
+###Invoke functionality through main
+def main():
+
+    #delete files from previous run 
+    clean_start()
+
+    #Call geometry 
+    geom()
+
+    #Call gun
+    x_pos, p_gen = gun(nTracks)
+
+    #Call truth plots 
+    truthPlots(x_pos, p_gen)
+
+    #Tracking
+    track()
+
+    #Call track plots
+    trackPlots()
+
+
+
+###Define functions
+def geom():
+    pass
+    #return stationGeom 
+
+def gun(nTracks):
+    print("Generating", nTracks, "tracks")
+    
+    #draw position from a UD 
+    x_pos = np.random.uniform(-1, 1, nTracks)*X_spread
+    
+    #generate momentum (beam direction)
+    p_gen = np.random.uniform(0, 1, nTracks)*P_max 
+
+    P = [p_gen, 0] 
+    
+        
+    return x_pos, p_gen
+
+def truthPlots(x_pos, p_gen):
+    histo(x_pos, "Generated X Position", "m")
+    histo(p_gen, "Generated P", "MeV")
+    pass
+
+def track():
+    pass 
+    #return recoTrajectory 
+
+def trackPlots():
+    pass 
+
+###Define helper functions
+def histo(X, hist_label, units):
+    # create 1 subplot
+    fig0, ax0 = plt.subplots(1,1, figsize=(5,5))
+    #create the histogram 
+    n0,b0,p0 = ax0.hist(X, bins='auto', color=COLOR)
+    ax0.set_xlabel(str(hist_label)+"/ "+str(units))
+    fig0.savefig(str(hist_label.replace(" ", "_"))+".png", dpi=250)
+
+def clean_start():
+    #clean old .png files  #TODO make a function 
+    glob_str="*.png"
+    subprocess.Popen(["trash"] + glob.glob(glob_str))
+
+
+
+#Define classes and data-types
+
+#Execute code through main 
+if __name__=="__main__":
+    main()
+
+
