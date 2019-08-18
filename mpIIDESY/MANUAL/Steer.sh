20170cc7f877a8b7dfb740a12972589c89b2f065
glukicov <g.lukicov@ucl.ac.uk>
2019-08-18 01:03:31 -0500

    Moving to collectionsNtuples

---
+++ mpIIDESY/MANUAL/Steer.sh
@@ -0,0 +1,6 @@
+* g-2 Tracker Alignment: PEDE Steering File
+
+ConstraintFile.txt ! Constraints text file
+Cfiles ! Following bin files are Cfiles
+Data.bin ! Binary data file
+method inversion 10 0.01 
