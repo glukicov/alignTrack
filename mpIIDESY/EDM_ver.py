f1c5bd3d1a42a3dfffcf8c018148f4aa8760a575
glukicov <g.lukicov@ucl.ac.uk>
2019-09-10 18:04:25 -0500

    DPF APS 2019

--- mpIIDESY/EDM_ver.py
+++ mpIIDESY/EDM_ver.py
@@ -1,11 +1,10 @@
 ### Gleb 
 # Looking into vertical beam from trackers 
-
-
 from os import listdir
 from os.path import isfile, join
 import argparse, sys
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.ticker import MaxNLocator
 from ROOT import TFile
 
@@ -28,40 +27,57 @@ tfiles = []
 for i_file in files:
     tfiles.append(TFile.Open(path+"/"+i_file))
 
+histArray=[] #1D vertical beam 
+
 # loop over files and stations
 run_numbers = []
 track_number = []
 vertical = [ [], []]
 vertical_error = [ [], []]
-for file in tfiles:
+for i_file, file in enumerate(tfiles):
     run_numbers.append(int(file.Get("TrackSummary/RunInfo/TracksPerEvent").GetMean()))
     track_number.append(file.Get("Extrapolation/vertices/station12/h_verticalPos").GetEntries())
     for i_station, station in enumerate(stations):
-        vertical[i_station].append(file.Get("Extrapolation/vertices/station"+str(station)+"/h_verticalPos").GetMean())
-        vertical_error[i_station].append(file.Get("Extrapolation/vertices/station"+str(station)+"/h_verticalPos").GetMeanError())
+        # histo_1D_noCut=file.Get("Extrapolation/vertices/station"+str(station)+"/h_verticalPos")
+        histo_2D = file.Get("Extrapolation/vertices/station"+str(station)+"/h_verticalPos_vs_time")
+        first_bin = histo_2D.GetXaxis().FindBin(30.0) # 30 us 
+        tmpNameTH1 = "tmpNameTH1_"+str(i_file)+str(i_station) # assign a new "name pointer" to the TH1 object
+        histo_1D = histo_2D.ProjectionY(tmpNameTH1, first_bin, -1) 
+        histArray.append(histo_1D) # keep TH1D in scope if want them later 
+        vertical[i_station].append(histo_1D.GetMean())
+        vertical_error[i_station].append(histo_1D.GetMeanError())
 
-
-print("Run numbers:", run_numbers)
 print("Tracks #",track_number)
 
 ### Plotting 
 fig, ax = plt.subplots()
-for i_station, station in enumerate(stations):
-    ax.scatter(run_numbers, vertical[i_station], label="S"+station, color=colors[i_station])
-    plt.errorbar(run_numbers, vertical[i_station], yerr=vertical_error[i_station],  color=colors[i_station], markersize=12, elinewidth=1, linewidth=0)
+x_ticks=x =np.char.replace(files, '.root', '')
+
+# S12 and S18 
+# for i_station, station in enumerate(stations):
+#     ax.scatter(x_ticks, vertical[i_station], label="S"+station, color=colors[i_station])
+#     plt.errorbar(x_ticks, vertical[i_station], yerr=vertical_error[i_station],  color=colors[i_station], markersize=12, elinewidth=1, capsize=2, linewidth=0)
+
+# S18 - S12
+# y_title=r"$\Delta$ (S18-S12) Vertical [mm]"
+# label = r"$\Delta$ (S18-S12)"
+# color="green"
+# value = np.array(vertical[1])-np.array(vertical[0])
+# error = np.sqrt( np.array(vertical_error[1])**2 + np.array(vertical_error[0])**2 )
+
+# # <S18S12>
+# y_title=r"$\langle$S18S12$\rangle$ Vertical [mm]"
+# label = r"$\langle$S18S12$\rangle$"
+# color="green"
+# value =( np.array(vertical[1]) + np.array(vertical[0]) ) / 2 
+# error = np.sqrt( np.array(vertical_error[1])**2 + np.array(vertical_error[0])**2 )
+
+ax.scatter(x_ticks, value, label=label, color=color)
+plt.errorbar(x_ticks, value, yerr=error, color=color, markersize=12, elinewidth=1, capsize=2, linewidth=0)
 
 #massaging plot 
-ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-ax.get_xaxis().get_major_formatter().set_useOffset(False)
-ax.set_ylabel("Vertical Beam Position [mm]", fontsize=14, fontweight='bold')
-ax.set_xlabel("Run #", fontsize=14, fontweight='bold')
+ax.set_ylabel(y_title, fontsize=14, fontweight='bold')
+ax.set_xlabel("Setting", fontsize=14, fontweight='bold')
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 12}) # outside (R) of the plot 
-plt.tight_layout(pad=0.4, w_pad=0.1, h_pad=1.0)
-
-plt.savefig("vertical_run.png", dpi=200)
-
-
-
-
-
-
+plt.tight_layout()
+plt.savefig("vertical_run.png", dpi=100)
