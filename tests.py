a39abb768ab414164a9282057776807082e8b914
glukicov <g.lukicov@ucl.ac.uk>
2020-03-08 23:24:10 +0000

    Added Travis build tools

---
+++ tests.py
@@ -0,0 +1,17 @@
+'''
+Define some unit test and integration tests
+'''
+import numpy as np
+
+def main():
+    print("tests.py::Starting unit tests")
+    
+    test_res()
+
+    print("tests.py::Everything passed")
+
+def test_res():
+    pass
+
+if __name__ == "__main__":
+    main()
